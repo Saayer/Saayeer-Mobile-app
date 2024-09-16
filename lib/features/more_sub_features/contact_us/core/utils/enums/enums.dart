89242c5067e528de70f1aa89e498a03ea4b3b185
A-Elbadawy <elbadawy200997@gmail.com>
2024-09-16 21:28:49 +0300

    Modify Contact us page

--- lib/features/more_sub_features/contact_us/core/utils/enums/enums.dart
+++ lib/features/more_sub_features/contact_us/core/utils/enums/enums.dart
@@ -1,22 +1,40 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 
 enum ContactUsTypes {
-  WHATSAPP._(
-      title: "whats_app",
-      description: "00009789",
-      iconData: LineIcons.whatSApp),
-  HOTLINE._(title: "hotline", description: "0600", iconData: LineIcons.phoneVolume),
+  WHATSAPP._(title: "whats_app", description: "00009789", clickable: false, url: '', iconData: LineIcons.whatSApp),
+  HOTLINE._(title: "hotline", description: "0600", clickable: false, url: '', iconData: LineIcons.phoneVolume),
   FACEBOOK._(
-      title: "facebook", description: "saayer", iconData: LineIcons.facebookSquare),
+      title: "facebook",
+      description: "saayer",
+      clickable: true,
+      url: 'https://www.facebook.com/profile.php?id=61556770131235&mibextid=LQQJ4d',
+      iconData: LineIcons.facebookSquare),
+  TWITTER._(
+      title: "twitter", description: "saayer", clickable: true, url: 'https://x.com/_saayer', iconData: LineIcons.twitter),
   INSTAGRAM._(
-      title: "instagram", description: "saayer", iconData: LineIcons.instagram),
-  WEBSITE._(title: "website", description: "saayer", iconData: Icons.language);
+      title: "instagram",
+      description: "saayer",
+      clickable: true,
+      url: 'https://www.instagram.com/_saayer_',
+      iconData: LineIcons.instagram),
+  TIKTOK._(
+      title: "tiktok",
+      description: "saayer",
+      clickable: true,
+      url: 'https://www.tiktok.com/@.saayer',
+      iconData: Icons.tiktok),
+  WEBSITE._(title: "website", description: "saayer", clickable: true, url: 'https://saayer.com/', iconData: Icons.language);
 
-  final String title, description;
+  final String title, description, url;
   final IconData iconData;
+  final bool clickable;
 
-  const ContactUsTypes._(
-      {required this.title, required this.description, required this.iconData});
+  const ContactUsTypes._({
+    required this.title,
+    required this.description,
+    required this.iconData,
+    required this.clickable,
+    required this.url,
+  });
 }
