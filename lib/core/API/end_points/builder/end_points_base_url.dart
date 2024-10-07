56f0c4f2f5e3ed13e2214973f0de241511688b1e
A-Elbadawy <elbadawy200997@gmail.com>
2024-10-07 10:47:19 +0300

    change http to https

--- lib/core/API/end_points/builder/end_points_base_url.dart
+++ lib/core/API/end_points/builder/end_points_base_url.dart
@@ -14,17 +14,17 @@ class EndPointsBaseUrl {
       case FlavorType.DEV:
         {
           return const EndPointsBaseUrl._internal(
-              baseUrl: "http://34.140.10.214/saayer-6/dev");
+              baseUrl: "https://app.saayer.com/saayer-6/dev");
         }
       case FlavorType.STAGING:
         {
           return const EndPointsBaseUrl._internal(
-              baseUrl: "http://34.140.10.214/saayer-6/stag");
+              baseUrl: "https://app.saayer.com/saayer-6/stag");
         }
       case FlavorType.PROD:
         {
           return const EndPointsBaseUrl._internal(
-              baseUrl: "http://34.140.10.214/saayer-6/dev");
+              baseUrl: "https://app.saayer.com/saayer-6/dev");
         }
     }
   }
