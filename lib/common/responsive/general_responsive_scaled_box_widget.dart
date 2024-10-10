ddda662ba1ff7e2bafb9e9558603ccee24f960c7
A-Elbadawy <elbadawy200997@gmail.com>
2024-10-10 12:42:05 +0300

    modify Responsive values

--- lib/common/responsive/general_responsive_scaled_box_widget.dart
+++ lib/common/responsive/general_responsive_scaled_box_widget.dart
@@ -12,9 +12,9 @@ class GeneralResponsiveScaledBoxWidget extends StatelessWidget {
     return ResponsiveScaledBox(
       width: ResponsiveValue<double>(context, conditionalValues: const [
         Condition.between(start: 0, end: 450, value: 450),
-        Condition.between(start: 450, end: 800, value: 500),
-        Condition.between(start: 800, end: 1100, value: 800),
-        Condition.between(start: 1100, end: 1400, value: 1000),
+        Condition.between(start: 450, end: 800, value: 600),
+        //Condition.between(start: 800, end: 1100, value: 1000),
+        Condition.between(start: 800, end: 1400, value: 1000),
         Condition.between(start: 1400, end: 9999, value: 1500),
       ]).value,
       child: child,
